d10aa00fbbbcc2237e710ab15403a18e5657f5e9
ArjunCB28 <arjuncb28@gmail.com>
2019-11-23 14:20:09 -0400

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name silFormsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the silFormsApp
+ */
+angular.module('silFormsApp').controller('MainCtrl', function ($scope) {
+	$scope.name = 'Silforms';
+});
